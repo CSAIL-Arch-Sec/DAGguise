62daed8bb242c407b88d66bfdf3feb3a037c398c
Peter Deutsch <pwd@mit.edu>
2021-11-28 17:52:58 -0500

    Minor bugfixes

--- checkpoint_merge/generateMerge.sh
+++ checkpoint_merge/generateMerge.sh
@@ -7,5 +7,5 @@ localdir="${BASH_REMATCH[2]}"
 echo $localdir
 
 mkdir -p $1/$localdir
-python $GEM5_ROOT/util/checkpoint_aggregator.py -o $1/$localdir --memory-size=4294967296 --cpts $2 $3
+python2 $GEM5_ROOT/util/checkpoint_aggregator.py -o $1/$localdir --memory-size=4294967296 --cpts $2 $3
 
--- eval_scripts/simu_single/generate_runscript.py
+++ eval_scripts/simu_single/generate_runscript.py
@@ -11,25 +11,26 @@ assert(specroot is not None)
 
 rundir = "./run/"
 ckptdir = gem5root + "/checkpoint_merge/merged_checkpoint/"
+resultsdir = gem5root + "/eval_scripts/simu_simple/results/"
+dramsim2File = resultsdir + "dram"
+
 
 benchmark = 'bwaves_r'
 chkptNum = 0
 assert(len(sys.argv) > 1)
 
-destFile = 'runscript.sh'
+runscriptFile = 'runscript.sh'
 
-copyfile(sys.argv[1], destFile)
-runHandle = open(destFile, "a+")
+copyfile(sys.argv[1], runscriptFile)
+runHandle = open(runscriptFile, "a+")
 
 runHandle.write('\t--checkpoint-dir=/%s/\\\n' % ckptdir)
 runHandle.write('\t--benchmark='+benchmark+' \\\n')
 runHandle.write('\t--simpt-ckpt='+str(chkptNum)+' \\\n')
+runHandle.write('\t--dramsim2outputfile=%s \\\n' % (dramsim2File))
 
 runHandle.close()
 
-os.makedirs(rundir, exist_ok=True)
-os.makedirs(os.path.join(rundir, 'results', 'DDR3_micron_32M_8B_x8_sg125', '4GB.1Ch.8R.scheme2.close_page.32TQ.32CQ.RtB.pRankpBank'), exist_ok=True)
-subprocess.call(["sed", "-i", "-e", 's/DRAM_REPLACE/dram/g', destFile])
-subprocess.call(["sed", "-i", "-e", 's/OUTDIR_REPLACE/output/g', destFile])
-subprocess.call(["chmod", "+777", destFile])
+subprocess.call(["sed", "-i", "-e", 's|OUTDIR_REPLACE|%s|g' % resultsdir, runscriptFile])
+subprocess.call(["chmod", "+777", runscriptFile])
     
