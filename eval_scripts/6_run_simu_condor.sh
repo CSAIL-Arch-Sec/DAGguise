32e09c75c3b4d8aadb9cf4bf7c504b0e837283dd
Peter Deutsch <pwd@mit.edu>
2021-12-01 11:19:28 -0500

    Fixing some incorrect commits

--- eval_scripts/6_run_simu_condor.sh
+++ eval_scripts/6_run_simu_condor.sh
@@ -19,21 +19,21 @@ condor_submit $GEM5_ROOT/eval_scripts/simu_condor/launch.condor
 cd -
 
 # Then, execute the FS-BTA setup
-#export TEST_NAME="docDist_2cpu_FSBTA"
+export TEST_NAME="docDist_2cpu_FSBTA"
 
-#cd $GEM5_ROOT/eval_scripts/simu_condor/
-#python3 generateRunscripts.py $GEM5_ROOT/eval_scripts/simu_common/$TEST_NAME.sh
+cd $GEM5_ROOT/eval_scripts/simu_condor/
+python3 generateRunscripts.py $GEM5_ROOT/eval_scripts/simu_common/$TEST_NAME.sh
 
-#cd $SPEC_ROOT/
-#condor_submit $GEM5_ROOT/eval_scripts/simu_condor/launch.condor
-#cd -
+cd $SPEC_ROOT/
+condor_submit $GEM5_ROOT/eval_scripts/simu_condor/launch.condor
+cd -
 
 # Finally, execute the baseline
-#export TEST_NAME="docDist_2cpu_regular"
+export TEST_NAME="docDist_2cpu_regular"
 
-#cd $GEM5_ROOT/eval_scripts/simu_condor/
-#python3 generateRunscripts.py $GEM5_ROOT/eval_scripts/simu_common/$TEST_NAME.sh
+cd $GEM5_ROOT/eval_scripts/simu_condor/
+python3 generateRunscripts.py $GEM5_ROOT/eval_scripts/simu_common/$TEST_NAME.sh
 
-#cd $SPEC_ROOT/
-#condor_submit $GEM5_ROOT/eval_scripts/simu_condor/launch.condor
-#cd -
+cd $SPEC_ROOT/
+condor_submit $GEM5_ROOT/eval_scripts/simu_condor/launch.condor
+cd -
