0f9bd80213df0e578b26e805fc4919381fb56d11
Peter Deutsch <pwd@mit.edu>
2021-12-21 15:41:07 -0500

    Added warning if checkpoint is missing

--- eval_scripts/4_merge_checkpoint_condor.sh
+++ eval_scripts/4_merge_checkpoint_condor.sh
@@ -11,6 +11,8 @@ fi
 # Change if desired
 VICTIM_CHECKPOINT="$GEM5_ROOT/checkpoint/docdist/cpt.1285668325407/"
 
+[ ! -d $VICTIM_CHECKPOINT ] && echo "Specified checkpoint doesn't exist! The checkpoint pointer in this script may need to be updated to point to the correct path/tick id." && exit 1
+
 cd $GEM5_ROOT/checkpoint_merge/
 export GEM5_ROOT
 export VICTIM_CHECKPOINT
--- eval_scripts/4_merge_checkpoint_single.sh
+++ eval_scripts/4_merge_checkpoint_single.sh
@@ -12,6 +12,8 @@ fi
 victim_checkpoint="$GEM5_ROOT/checkpoint/docdist/cpt.1285668325407/"
 unprotected_checkpoint="$SPEC_ROOT/ckpt/bwaves_r/cpt.None.SIMP-0/"
 
+[ ! -d $victim_checkpoint ] && echo "Specified checkpoint doesn't exist! The checkpoint pointer in this script may need to be updated to point to the correct path/tick id." && exit 1
+
 cd $GEM5_ROOT/checkpoint_merge/
 export GEM5_ROOT
 bash generateMerge_single.sh merged_checkpoint/cpt.None.SIMP-0/ $unprotected_checkpoint $victim_checkpoint
