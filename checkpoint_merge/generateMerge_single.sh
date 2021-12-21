f0d08baf5a1899c36a56bca9de17b3422b1c41ed
Peter Deutsch <pwd@mit.edu>
2021-12-21 11:27:59 -0500

    Added missed python2 flag

--- checkpoint_merge/generateMerge_single.sh
+++ checkpoint_merge/generateMerge_single.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir -p $1
-python $GEM5_ROOT/util/checkpoint_aggregator.py -o $1 --memory-size=4294967296 --cpts $2 $3
+python2 $GEM5_ROOT/util/checkpoint_aggregator.py -o $1 --memory-size=4294967296 --cpts $2 $3
 
